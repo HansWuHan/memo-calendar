739f1eb2e8765982608de8f34c7a573af7514960
hanswuhan <hanswuhan@gmail.com>
2019-06-13 23:56:06 +0800

    calendar basic

---
+++ src/components/Calendar/Calendar.vue
@@ -0,0 +1,56 @@
+<template>
+	<div id="app">
+		<calendar-view
+      :events="events"
+      :show-date="showDate"
+      :time-format-options="{hour: 'numeric', minute:'2-digit'}"
+      :show-event-times="showEventTimes"
+			class="theme-han holiday-us-traditional holiday-us-official">
+			<calendar-view-header
+				slot="header"
+				slot-scope="t"
+				:header-props="t.headerProps"
+				@input="setShowDate" />
+		</calendar-view>
+	</div>
+</template>
+<script>
+	import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+	// The next two lines are processed by webpack. If you're using the component without webpack compilation,
+	// you should just create <link> elements for these. Both are optional, you can create your own theme if you prefer.
+	require("./style.css")
+	require("vue-simple-calendar/static/css/holidays-us.css")
+
+	export default {
+		name: 'app',
+		data: function() {
+      return { 
+        showDate: new Date(),
+        showEventTimes: true,
+
+      }
+    },
+    props: {
+      events: []
+    },
+		components: {
+			CalendarView,
+			CalendarViewHeader,
+		},
+		methods: {
+			setShowDate(d) {
+				this.showDate = d;
+			},
+		}
+	}
+</script>
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		color: #2c3e50;
+		height: 67vh;
+		width: 90vw;
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <Calendar :events="events"></Calendar>
+  </div>
+</template>
+
+<script>
+import Calendar from './Calendar/Calendar'
+export default {
+  name: 'HelloWorld',
+  components: { Calendar },
+  props: {
+    msg: String
+  },
+  data: function () {
+    return {
+      events: [
+        {
+          id: "e4",
+          startDate: new Date(),
+          title: "My Birthday!",
+          classes: "birthday",
+          url: "https://en.wikipedia.org/wiki/Birthday",
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
